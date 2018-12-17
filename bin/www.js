45aa1b188cdd1090fa9208e8cfa6b8f27ad7d6a5
Fanning <fanning.meng@unspay.com>
2018-12-17 18:14:32 +0800

    ..

--- bin/www.js
+++ bin/www.js
@@ -5,5 +5,5 @@ const app = require('../app.js')
 const service = http.createServer(app.callback())
 
 service.listen(3000, () => {
-  console.log(`please listen at ${os.networkInterfaces().lo[0].address}:3000`)
+  console.log(`please listen at http://${os.networkInterfaces().lo[0].address}:3000`)
 })
